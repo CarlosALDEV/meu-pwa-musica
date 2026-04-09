902f6bcd5419505e9692d19c4baf57cee3711785
CarlosALDEV <contato.carlosalves23@gmail.com>
2026-04-09 15:59:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = 'music-pwa-v1';
+const CACHE_NAME = 'music-pwa-v2';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
-      .catch(() => {})
+      .catch(err => console.log('Erro ao fazer cache', err))
   );
   self.skipWaiting();
 });
@@ -35,6 +36,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
+      .catch(() => caches.match('./index.html'))
   );
 });
